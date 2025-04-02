f7b2d9462c46254deeddcc50fdb6100063669f95
aradhana33 <153969600+aradhana33@users.noreply.github.com>
2025-04-01 22:49:17 -0400

    Update urls.py

--- health_income_proj/core/urls.py
+++ health_income_proj/core/urls.py
@@ -8,9 +8,13 @@
 # ]
 
 from django.urls import path
-from .views import dashboard_from_excel
+from . import views
 
 urlpatterns = [
-    path('dashboard/', dashboard_from_excel, name='dashboard_excel'),
+    path('dashboard/', views.dashboard_from_excel, name='dashboard_excel'),
     #path('hypothesis1/', hypothesis_one_view, name='hypothesis_one'),
+    path('api/gender-pay-gap/', views.api_gender_pay_gap),
+    path('api/province-income-health/', views.api_province_income_health),
+    path('api/work-mental/', views.api_work_vs_mental),
+    path('api/assistance-food/', views.api_assistance_vs_food),
 ]
