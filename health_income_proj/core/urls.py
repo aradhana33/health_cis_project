2a2e4029ef57a2487475bfd9c6fd3f93185383ed
aradhana33 <153969600+aradhana33@users.noreply.github.com>
2025-04-08 16:48:36 -0400

    Update urls.py

--- health_income_proj/core/urls.py
+++ health_income_proj/core/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('dashboard/', views.dashboard_from_excel, name='dashboard_excel'),
     #path('hypothesis1/', hypothesis_one_view, name='hypothesis_one'),
+    path('gender-pay-gap/', views.gender_pay_gap_chart),
     path('api/gender-pay-gap/', views.api_gender_pay_gap),
     path('api/province-income-health/', views.api_province_income_health),
     path('api/work-mental/', views.api_work_vs_mental),
